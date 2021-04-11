e9b99dd5b19f2f38abe3a76e94924c8dfdb644a9
alperenkbd <alperen_kabadayi@hotmail.com>
2021-04-12 01:03:00 +0300

    add feature of distance and angle between plane and enemy

---
+++ Assets/Scripts/GameScripts/GameUi/Radar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radar : MonoBehaviour
+{
+    [SerializeField] GameObject fighter;
+    [SerializeField] GameObject enemy;
+    private Vector3 fighterCoordinate;
+    private Vector3 enemyCoordinate;
+    private Vector3 targetDir;
+
+    private void Update()
+    {
+        DistanceCalculator();
+    }
+
+    private void DistanceCalculator()
+    {
+        float distance;
+        float angle;
+        
+        fighterCoordinate = fighter.transform.position;
+        enemyCoordinate = enemy.transform.position;
+        targetDir = enemy.transform.position - fighter.transform.position;
+        angle = Vector3.SignedAngle(targetDir, -fighter.transform.forward,-Vector3.forward);
+        distance = Vector3.Distance(enemyCoordinate , fighterCoordinate);
+        Debug.Log("düşmanın uzaklığı: " +distance+ "düşmanın açısı: "+ angle);
+
+
+    }
+
+    
+
+}
