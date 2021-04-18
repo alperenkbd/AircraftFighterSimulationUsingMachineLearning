47ce5d5a890dc48036a8a13e48a8c95bda6b5cf0
alperenkbd <alperen_kabadayi@hotmail.com>
2021-04-18 05:24:13 +0300

    changed anchoredposition for radar feature

--- Assets/Scripts/GameScripts/GameUi/Radar.cs
+++ Assets/Scripts/GameScripts/GameUi/Radar.cs
@@ -1,35 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Radar : MonoBehaviour
 {
     [SerializeField] GameObject fighter;
     [SerializeField] GameObject enemy;
+    [SerializeField] RawImage enemyImg;
+    private RectTransform enemyRectTransform;
     private Vector3 fighterCoordinate;
     private Vector3 enemyCoordinate;
     private Vector3 targetDir;
 
+
+    private void Start()
+    {
+        enemyRectTransform = enemyImg.transform.GetComponent<RectTransform>();
+    }
+
+
     private void Update()
     {
         DistanceCalculator();
+        //AngleCalculator();
+        EnemyPositioner();
     }
 
-    private void DistanceCalculator()
+    private float DistanceCalculator()
     {
         float distance;
-        float angle;
         
         fighterCoordinate = fighter.transform.position;
         enemyCoordinate = enemy.transform.position;
-        targetDir = enemy.transform.position - fighter.transform.position;
-        angle = Vector3.SignedAngle(targetDir, -fighter.transform.forward,-Vector3.forward);
         distance = Vector3.Distance(enemyCoordinate , fighterCoordinate);
-        Debug.Log("düşmanın uzaklığı: " +distance+ "düşmanın açısı: "+ angle);
 
-
+        return distance;
     }
 
-    
+    private float AngleCalculator()
+    {
+        float angle;
+        targetDir = enemy.transform.position - fighter.transform.position;
+        angle = Vector3.SignedAngle(targetDir, -fighter.transform.forward, -Vector3.forward);
+
+        return angle;
+    }
+
+    private void EnemyPositioner()
+    {
+        float localDistance;
+
+        localDistance = DistanceCalculator() / 100f;
+        
+        if(localDistance > 70)
+        {
+            enemyImg.gameObject.SetActive(false);
+        }
+        else
+        {
+            enemyImg.gameObject.SetActive(true);
+            Debug.Log(localDistance);
+            
+            enemyRectTransform.anchoredPosition = new Vector3(enemyRectTransform.anchoredPosition.x, localDistance, enemyRectTransform.anchoredPosition3D.z);
+            
+
+        }
+    }
 
 }
